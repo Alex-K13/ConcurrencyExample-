e7cd4e129ad2991460df1f4b4d998dfb2d691379
Alexandr 13K <kuchersasha.ruz@gmail.com>
2020-03-24 14:32:37 +0200

    done concurrency part of hw

--- src/main/database/DataBase.java
+++ src/main/database/DataBase.java
@@ -1,4 +1,49 @@
 package main.database;
 
-public class DataBase {
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class DataBase implements Runnable {
+
+    public static Map<LocalDate, Integer> dateOfPaymentMap = new HashMap<>();
+
+    public void put(LocalDate date, Integer paymentAmount) {
+        ReentrantLock lock = new ReentrantLock();
+        lock.lock();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+        dateOfPaymentMap.put(date, paymentAmount);
+        System.out.println("Next index" + dateOfPaymentMap);
+        lock.lock();
+    }
+
+    public Integer getByDate(LocalDate date) {
+        Integer paymentByDate = dateOfPaymentMap.get(date);
+        if (paymentByDate == null) {
+            System.out.println("There are no payment for this date");
+        } else {
+            System.out.println("The payment for this day is: " + paymentByDate);
+        }
+        return paymentByDate;
+    }
+
+    public void outputDates() {
+        System.out.println(dateOfPaymentMap.keySet());
+    }
+
+    public void outputPayment() {
+        System.out.println(dateOfPaymentMap.values());
+    }
+
+    @Override
+    public void run() {
+        outputDates();
+        outputPayment();
+        getByDate(LocalDate.of(2020, 3, 24));
+    }
 }
